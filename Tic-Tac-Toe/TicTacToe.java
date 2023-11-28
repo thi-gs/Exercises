703dcb09b07bc8217bc26362ce0298aadc677362
thi-gs <thiago00gomes@hotmail.com>
2023-11-28 17:22:02 -0300

    The game now announces the winner and doesn't allow a player enters a number already marked

--- Tic-Tac-Toe/TicTacToe.java
+++ Tic-Tac-Toe/TicTacToe.java
@@ -14,28 +14,51 @@ public class TicTacToe {
 
         boolean gameOver = gameValid(ticTacToe);
 
+        boolean approvedPlay = false;
+
         while(gameOver == false) {
             System.out.println();
             printTable(ticTacToe);
             System.out.println();
+
             if(round % 2 != 0){
-                System.out.println("X round = choose a number: ");
-                Scanner myObj = new Scanner(System.in);
-                String numberChosed = myObj.nextLine();
-                markNumberX(numberChosed, ticTacToe);
+                while (approvedPlay != true){
+                    System.out.println("X round = choose a number: ");
+                    Scanner myObj = new Scanner(System.in);
+                    String numberChosed = myObj.nextLine();
+                    approvedPlay = markNumberX(numberChosed, ticTacToe);
+                    if(approvedPlay == false){
+                        System.out.println();
+                        printTable(ticTacToe);
+                        System.out.println();
+                    }
+                }
                 gameOver = gameValid(ticTacToe);
+                approvedPlay = false;
             } else if (round % 2 == 0) {
-                System.out.println("0 round = choose a number: ");
-                Scanner myObj = new Scanner(System.in);
-                String numberChosed2 = myObj.nextLine();
-                markNumber0(numberChosed2, ticTacToe);
+                while (approvedPlay != true){
+                    System.out.println("0 round = choose a number: ");
+                    Scanner myObj = new Scanner(System.in);
+                    String numberChosed2 = myObj.nextLine();
+                    approvedPlay = markNumber0(numberChosed2, ticTacToe);
+                    if(approvedPlay == false){
+                        System.out.println();
+                        printTable(ticTacToe);
+                        System.out.println();
+                    }
+                }
                 gameOver = gameValid(ticTacToe);
+                approvedPlay = false;
             }
             round = round + 1;
         }
 
         printTable(ticTacToe);
-        System.out.println("Game Over!");
+        if(round % 2 == 0){
+            System.out.println("Game Over! X won!");
+        } else {
+            System.out.println("Game Over! 0 won!");
+        }
     }
 
     private static boolean gameValid(String[][] ticTacToe) {
@@ -152,67 +175,195 @@ public class TicTacToe {
         }
     }
 
-    private static void markNumberX(String numberChosed, String[][] ticTacToe) {
+    private static boolean markNumberX(String numberChosed, String[][] ticTacToe) {
         switch (numberChosed) {
             case "1":
-                ticTacToe[0][0] = "X";
-                break;
+                if(ticTacToe[0][0].equals("1")){
+                    ticTacToe[0][0] = "X";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
             case "2":
-                ticTacToe[0][1] = "X";
-                break;
+                if(ticTacToe[0][1].equals("2")){
+                    ticTacToe[0][1] = "X";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
             case "3":
-                ticTacToe[0][2] = "X";
-                break;
+                if(ticTacToe[0][2].equals("3")){
+                    ticTacToe[0][2] = "X";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
             case "4":
-                ticTacToe[1][0] = "X";
-                break;
+                if(ticTacToe[1][0].equals("4")){
+                    ticTacToe[1][0] = "X";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
             case "5":
-                ticTacToe[1][1] = "X";
-                break;
+                if(ticTacToe[1][1].equals("5")){
+                    ticTacToe[1][1] = "X";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
             case "6":
-                ticTacToe[1][2] = "X";
-                break;
+            if(ticTacToe[1][2].equals("6")){
+                    ticTacToe[1][2] = "X";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
             case "7":
-                ticTacToe[2][0] = "X";
-                break;
+                if(ticTacToe[2][0].equals("7")){
+                    ticTacToe[2][0] = "X";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
             case "8":
-                ticTacToe[2][1] = "X";
-                break;
+                if(ticTacToe[2][1].equals("8")){
+                    ticTacToe[2][1] = "X";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
             case "9":
-                ticTacToe[2][2] = "X";
-                break;
+                if(ticTacToe[2][2].equals("9")){
+                    ticTacToe[2][2] = "X";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
         }
+        return false;
     }
 
-    private static void markNumber0(String numberChosed2, String[][] ticTacToe) {
+    private static boolean markNumber0(String numberChosed2, String[][] ticTacToe) {
         switch (numberChosed2) {
             case "1":
+            if(ticTacToe[0][0].equals("1")){
                 ticTacToe[0][0] = "0";
+                return true;
+                //break;
+            } else {
+                System.out.println("Invalid Play! Please, try again!");
+                //return false;
                 break;
+            }
             case "2":
-                ticTacToe[0][1] = "0";
-                break;
+                if(ticTacToe[0][1].equals("2")){
+                    ticTacToe[0][1] = "0";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
             case "3":
-                ticTacToe[0][2] = "0";
-                break;
+                if(ticTacToe[0][2].equals("3")){
+                    ticTacToe[0][2] = "0";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
             case "4":
-                ticTacToe[1][0] = "0";
-                break;
+                if(ticTacToe[1][0].equals("4")){
+                    ticTacToe[1][0] = "0";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
             case "5":
-                ticTacToe[1][1] = "0";
-                break;
+                if(ticTacToe[1][1].equals("5")){
+                    ticTacToe[1][1] = "0";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
             case "6":
-                ticTacToe[1][2] = "0";
-                break;
+                if(ticTacToe[1][2].equals("6")){
+                    ticTacToe[1][2] = "0";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
             case "7":
-                ticTacToe[2][0] = "0";
-                break;
+                if(ticTacToe[2][0].equals("7")){
+                    ticTacToe[2][0] = "0";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
             case "8":
-                ticTacToe[2][1] = "0";
-                break;
+                if(ticTacToe[2][1].equals("8")){
+                    ticTacToe[2][1] = "0";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
             case "9":
-                ticTacToe[2][2] = "0";
-                break;
+                if(ticTacToe[2][2].equals("9")){
+                    ticTacToe[2][2] = "0";
+                    return true;
+                    //break;
+                } else {
+                    System.out.println("Invalid Play! Please, try again!");
+                    //return false;
+                    break;
+                }
         }
+        return false;
     }
 }
